71a5c39aaeec245c12efccae954d621be317a162
spike vargas <marc.lombry@gmail.com>
2019-05-23 15:48:51 +0200

    creation of models and migrations

---
+++ database/migrations/2019_05_23_110509_create_treatments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTreatmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('medicalprescription');
+            $table->unsignedInteger('disease_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treatments');
+    }
+}
